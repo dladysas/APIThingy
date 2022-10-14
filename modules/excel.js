b87cdbcedfefd4e42a45e022fb720ff0d3188b9c
pagan <ladysas.donatas@gmail.com>
2022-10-14 17:26:40 +0300

    add export report

--- modules/excel.js
+++ modules/excel.js
@@ -4,7 +4,7 @@ XLSX.set_fs(fs);
 
 async function report(data) {
   const wb = XLSX.utils.book_new();
-  const ws = XLSX.utils.json_to_sheet(data);
+  const ws = XLSX.utils.json_to_sheet(JSON.parse(JSON.stringify(data)));
   XLSX.utils.book_append_sheet(wb, ws, "Report");
   XLSX.writeFile(wb, "report.csv");
   return ws
