2cb471e252270fbc45e291ced030b7bf0a192509
pagan <ladysas.donatas@gmail.com>
2022-10-15 20:31:08 +0300

    add collumn width

--- modules/excel.js
+++ modules/excel.js
@@ -5,8 +5,20 @@ XLSX.set_fs(fs);
 async function report(data) {
   const wb = XLSX.utils.book_new();
   const ws = XLSX.utils.json_to_sheet(JSON.parse(JSON.stringify(data)));
+  ws['!cols'] = [
+    {wch: 10},
+    {wch: 10},
+    {wch: 10},
+    {wch: 10},
+    {wch: 22},
+    {wch: 10},
+    {wch: 12},
+    {wch: 12},
+    {wch: 22},
+    {wch: 22},
+  ];
   XLSX.utils.book_append_sheet(wb, ws, "Report");
-  XLSX.writeFile(wb, "report.csv");
+  XLSX.writeFile(wb, "report.xlsx");
   return ws
 }
 
