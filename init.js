7b5cd00b1e7baa39e28432e2d5acc741903d439f
pagan <ladysas.donatas@gmail.com>
2022-10-17 00:16:12 +0300

    add 2 commands

--- init.js
+++ init.js
@@ -4,33 +4,39 @@ import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers';
 import  _Gitlab  from './modules/gitlab.js'
 import report from './modules/excel.js'
+import _Jira from './modules/jira.js'
 
 
 
 yargs(hideBin(process.argv))
     .command('list', 'fetch all of the projects by current user', () => { }, async () => {
-        console.info(await _Gitlab.getAllUserProjects())
-        report(await _Gitlab.getAllUserProjects())
+        console.info(await _Gitlab.getAllUserProjects(), await _Jira.getAllBoards())
+        report(await _Gitlab.getAllUserProjects()) //await _Jira.getAllBoards())
     })
     .command('overview <id>', 'get overview of specific project', () => { }, async (argv) => {
         console.info(await _Gitlab.getProjectOverview(argv.id))
         report(await _Gitlab.getProjectOverview(argv.id))
     })
-    .command('milestone <id> [milestone]', 'get overview of specific project milestone', () => { }, async (argv) => {
-        if (argv.milestone) console.info(await getMilestoneOverview(argv.id, argv.milestone))
-        console.info(await getProjectMilestones(argv.id))
-    })
     .command('MR <id> [MR]', 'get merge requests for specific project', () => { }, async (argv) => {
-        if (argv.milestone) console.info(await getProjectOverview(argv.id, argv.mergeRequests))
-        console.info(await getMergeRequests(argv.id))
+        if (argv.mergeRequests) console.info(await _Gitlab.getProjectOverview(argv.id, argv.mergeRequests))
+        console.info(await _Gitlab.getMergeRequests(argv.id))
+        report(await _Gitlab.getMergeRequests(argv.id))
     })
-    .command('MRO <id> [MRO]', 'get overview of specific MR', () => { }, async (argv) => {
-        if (argv.mergeRequests) console.info(await getMergeRequests(argv.id, argv.mergeRequests))
-        console.info(await getMergeRequestOverview(argv.id))
+    //get overview of a specific merge request
+    .command('MRO <id> ', 'get overview of specific merge request', () => { }, async (argv) => {
+        if (argv.mergeRequestsOverview) console.info(await _Gitlab.getMergeRequests(argv.id, argv.mergeRequestsOverview))
+        console.info(await _Gitlab.getMergeRequestOverview(argv.id))
+        report(await _Gitlab.getMergeRequests(argv.id))
     })
-    .command('notes <id> [MRC]', 'get note count for MR', () => { }, async (argv) => {
-        if (argv) console.info(await getProjectIssueNotes(argv.id, argv))
-        console.info(await (argv.id))
+    .command('issues <id> [issues]', 'get issues for specific project', () => { }, async (argv) => {
+        if (argv.issues) console.info(await _Gitlab.getProjectOverview(argv.id, argv.issues))
+        console.info(await _Gitlab.getProjectIssues(argv.id))
+        report(await _Gitlab.getProjectIssues(argv.id))
+    })
+    //get overview of a specific issue
+    .command('statistics <id> ', 'get overview of specific issue', () => { }, async (argv) => {
+        if (argv.issue) console.info(await _Gitlab.getIssuesStatistics(argv.isue))
+        console.info(await _Gitlab.getIssuesStatistics(argv.id))
     })
     .demandCommand(1, 'You need at least one command before moving on')
     .strictCommands()
