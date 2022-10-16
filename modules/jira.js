a7ae93a7c16ed5da103cf7e11ed8168ebdf9c94e
pagan <ladysas.donatas@gmail.com>
2022-10-16 20:49:49 +0300

    change of npm package

--- modules/jira.js
+++ modules/jira.js
@@ -1,73 +1,78 @@
-import JiraApi from 'jira-client';
+// import JiraApi from 'jira-client';
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers';
+import { AgileClient } from 'jira.js';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-const jira = new JiraApi({
-    protocol: 'https',
-    host: 'donataspagan.atlassian.net',
-    username: 'ladysas.donatas@gmail.com',
-    password: 'l2Um5Rr3hv8qy8dJGfZUAC50',
-    apiVersion: '2',
-    strictSSL: true
-  });
+const host = process.env.JIRA_HOST;
+const email = process.env.JIRA_EMAIL;
+const token = process.env.JIRA_TOKEN;
 
-async function listProjects() {
-  const projects = await jira.listProjects({})
-  let repos = []
-  projects.forEach(project => {
-    const _project = {
-      id: project.id,
-      key: project.key,
-      name: project.name,
-    }
-    repos.push(_project)
-  }); 
-  return repos
-}
+const client = new AgileClient({
+  host: 'https://donataspagan.atlassian.net',
+  authentication: {
+    basic: {
+      email: 'ladysas.donatas@gmail.com',
+      apiToken: 'KY6NqcLmflBwsHnmJzeoA229',
+    },
+  },
+});
 
-async function getProjectByKey(key) {
-  const project = await jira.getProject(key)
+
+async function getAllProjects() {
+  const projects = await client.project.getAllProjects();
   return {
-    id: project.id,
-    key: project.key,
-    name: project.name,
-    assigneeType: project.assigneeType,
-    description: project.description,
-  }
+    projects: projects.values.map((project) => ({
+      id: project.id,
+      name: project.name,
+    })),
+  };
 }
 
-//create class get issues for a project
-class Issues {
-  constructor(key) {
-    this.key = key
-  }
-  async getIssues() {
-    const issues = await jira.searchJira(`project=${this.key}`)
-    return issues.issues
-  }
-  async getOpenIssues() {
-    const issues = await jira.searchJira(`project=${this.key} AND status != Closed`)
-    return issues.issues
-  }
-  async getClosedIssues() {
-    const issues = await jira.searchJira(`project=${this.key} AND status = Closed`)
-    return issues.issues
+async function getAllBoards() {
+  const boards = await client.board.getAllBoards();
+  return {
+    boards: boards.values.map((board) => ({
+      id: board.id,
+      name: board.name,
+      type: board.type,
+    })),
   }
 }
+// const jira = new JiraApi({
+//     protocol: 'https',
+//     host: 'donataspagan.atlassian.net',
+//     username: 'ladysas.donatas@gmail.com',
+//     password: 'l2Um5Rr3hv8qy8dJGfZUAC50',
+//     apiVersion: '2',
+//     strictSSL: true
+//   });
 
-const issues = new Issues()
+// async function listProjects() {
+//   const projects = await jira.listProjects({})
+//   let repos = []
+//   projects.forEach(project => {
+//     const _project = {
+//       id: project.id,
+//       key: project.key,
+//       name: project.name,
+//     }
+//     repos.push(_project)
+//   }); 
+//   return repos
+// }
 
-  
-yargs(hideBin(process.argv))
-  .command('list', 'fetch all projects', () => {}, async () => {
-    console.info(await listProjects())
-  })
-  .command('overview <key>', 'fetch project overview', () => {}, async (argv) => {
-    console.info(await getProjectByKey(argv.key, argv.listProjects))
-  })
-  .command('issues <key>', 'fetch all issues for a project', () => {}, async (argv) => {
-    console.info(await issues.getIssues(argv.key, argv.listProjects))
-  })  
-  .demandCommand(1, 'You need at least one command before moving on')
-  .strictCommands()
-  .parse()
+export default {
+  client,
+  getAllBoards
+}
+
+
+// yargs(hideBin(process.argv))
+//   .command('list', 'fetch all projects', () => {}, async () => {
+//     console.info(await getAllBoards())
+//   })
+//   .demandCommand(1, 'You need at least one command before moving on')
+//   .strictCommands()
+//   .parse()
